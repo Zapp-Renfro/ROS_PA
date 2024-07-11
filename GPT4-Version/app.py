1c4884c82d0a1c0ebd0e143295e33cb918a6cc0e
Ener1815 <thomas01.breton@gmail.com>
2024-07-12 00:32:57 +0200

    texte

--- GPT4-Version/app.py
+++ GPT4-Version/app.py
@@ -524,6 +524,7 @@ def get_results(job_id):
         return "Still processing", 202
 
 
+
 @app.route('/api/generate_text', methods=['POST'])
 def api_generate_text():
     data = request.get_json()
