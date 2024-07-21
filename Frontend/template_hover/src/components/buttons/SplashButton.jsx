45071b583ef4ff9150adab1a1806ec6c641a95b7
theod <theodalex93460@gmail.com>
2024-07-21 19:44:22 +0200

    on relie a index.html

--- Frontend/template_hover/src/components/buttons/SplashButton.jsx
+++ Frontend/template_hover/src/components/buttons/SplashButton.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { twMerge } from "tailwind-merge";
 
-export const SplashButton = ({ children, className, ...rest }) => {
+export const SplashButton = ({ children, className, href, ...rest }) => {
+  const Component = href ? 'a' : 'button';
+
   return (
-    <button
+    <Component
+      href={href}
       className={twMerge(
         "rounded-md bg-gradient-to-br from-blue-400 to-blue-700 px-4 py-2 text-lg text-zinc-50 ring-2 ring-blue-500/50 ring-offset-2 ring-offset-zinc-950 transition-all hover:scale-[1.02] hover:ring-transparent active:scale-[0.98] active:ring-blue-500/70",
         className
@@ -11,6 +14,6 @@ export const SplashButton = ({ children, className, ...rest }) => {
       {...rest}
     >
       {children}
-    </button>
+    </Component>
   );
 };
